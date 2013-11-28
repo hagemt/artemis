0aeb29ac164f610895c50e7d1a944a2f5b6be961
Tor E Hagemann <hagemt@rpi.edu>
2013-11-28 13:16:53 -0600

    Track next-generation additions to libartemis

---
+++ core/etc/epoll.c
@@ -0,0 +1,119 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <editline/readline.h>
+#include <pthread.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+#define MAX_EVENTS 32
+#define MAX_LINE 1024
+
+#define _(X) X
+#define NAME(X) #X
+#define WRAP(X) NAME(X)
+#define LOG2(TARGET,BODY) \
+	fprintf(TARGET, "[%s:%d] %s (%s)\n", \
+			__FILE__, __LINE__, _(BODY), strerror(errno));
+#define FIXME(X) LOG2(stderr, X)
+#define FD(X,FUNC,...) \
+	if ((X = FUNC(__VA_ARGS__)) < 0) { FIXME(WRAP(FUNC)); return X; }
+#define TRY(FUNC,...) \
+	if (FUNC(__VA_ARGS__)) { FIXME(WRAP(FUNC)); }
+
+struct listener_t {
+	int fd, pfd;
+	pthread_t tid;
+	struct epoll_event event;
+	struct epoll_event *events;
+};
+
+typedef struct listener_t Listener;
+
+static int
+accept_events(FILE *file, Listener *epoll)
+{
+	assert(file && epoll);
+	/* initialize fd */
+	FD(epoll->fd, fileno, file);
+	/* initialize polled fd */
+	FD(epoll->pfd, epoll_create1, EPOLL_CLOEXEC);
+	/* initialize epoll proper */
+	epoll->event.data.fd = epoll->fd;
+	epoll->event.events = EPOLLIN | EPOLLET;
+	TRY(epoll_ctl, epoll->pfd, EPOLL_CTL_ADD, epoll->fd, &epoll->event);
+	epoll->events = calloc(MAX_EVENTS, sizeof(epoll->event));
+	return (epoll->events == NULL);
+}
+
+static void *
+handle_prompt(void *data)
+{
+	ssize_t bytes, chars;
+	char buffer[MAX_LINE], *line;
+	Listener *epoll = (Listener *) data;
+	while ((line = readline(TARGET "> "))) {
+		chars = snprintf(buffer, MAX_LINE, "[LINE] %s\n", line);
+		bytes = write(epoll->fd, buffer, chars);
+		free(line);
+		assert(bytes == chars);
+	}
+	return data;
+}
+
+static int
+listen_onfifo(const char *target)
+{
+	int i, n;
+	FILE *file;
+	Listener epoll;
+	assert(target);
+	/* ensure file exists, reads from start, write appends */
+	file = fopen(target, "a+");
+	if (!file) { return 1; }
+	/* create some epoll magic */
+	TRY(accept_events, file, &epoll);
+	/* send I/O and wait for trigger */
+	TRY(pthread_create, &epoll.tid, NULL, &handle_prompt, &epoll);
+	while (pthread_tryjoin_np(epoll.tid, NULL)) {
+		n = epoll_wait(epoll.pfd, epoll.events, MAX_EVENTS, 1000);
+		for (i = 0; i < n; ++i) { /* bang */ fputc('\a', stderr); }
+	}
+	/* cleanup this awful mess */
+	free(epoll.events);
+	TRY(fclose, file);
+	return 0;
+}
+
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#ifndef EPOLL_FIFO
+#define EPOLL_FIFO "fifo"
+#endif /* EPOLL_FIFO */
+
+int
+main(int argc, char *argv[])
+{
+	struct stat info;
+	if (argc > 1) {
+		fprintf(stderr, "[USAGE] %s\n", name);
+		return EXIT_SUCCESS;
+	}
+	if (lstat(EPOLL_FIFO, &info)) {
+		fprintf(stderr, "[FATAL] couldn't stat %s (%s)\n",
+				EPOLL_FIFO, strerror(errno));
+		return EXIT_FAILURE;
+	}
+	if (!S_ISFIFO(info.st_type)) {
+		fprintf(stderr, "[FATAL] %s is not a FIFO (%s)\n",
+				EPOLL_FIFO, strerrno(errno));
+		return EXIT_FAILURE;
+	}
+	TRY(listen_onfifo, EPOLL_FIFO);
+	fputc('\n', stderr);
+	return EXIT_SUCCESS;
+}
---
+++ core/etc/inotify.c
@@ -0,0 +1,79 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <editline/readline.h>
+#include <pthread.h>
+#include <sys/inotify.h>
+#include <unistd.h>
+
+struct event_t {
+	int inotify_code;
+	const char *description;
+};
+
+typedef struct event_t Event;
+
+static Event event_types[] = {
+	{ IN_ACCESS,        "read of the file"                                  },
+	{ IN_MODIFY,        "last modification"                                 },
+	{ IN_ATTRIB,        "attributes of file change"                         },
+	{ IN_OPEN,          "open of file"                                      },
+	{ IN_CLOSE_WRITE,   "sent when a file opened for writing is closed"     },
+	{ IN_CLOSE_NOWRITE, "sent when a file opened not for writing is closed" },
+	{ IN_MOVED_FROM,    "when the file is moved or renamed"                 },
+	{ IN_MOVED_TO,      "when the file is moved or renamed"                 },
+	{ IN_DELETE,        "a file/directory deleted"                          },
+	{ IN_CREATE,        "a file in a watched directory is created"          },
+	{ IN_DELETE_SELF,   "file monitored is deleted"                         },
+	{ 0,                NULL                                                }
+};
+
+struct data_t {
+	pthread_t tid;
+};
+
+static void *
+handle(void *data) {
+	char *line;
+	struct data_t *stuff;
+	assert(data);
+	stuff = (struct data_t *) data;
+	while ((line = readline("> "))) {
+		free(line);
+	}
+	return data;
+}
+
+int
+main(void)
+{
+	Event *e;
+	char *line;
+	int fd, wd;
+	struct data_t data;
+	for (e = event_types; e->description; ++e) {
+		fprintf(stderr, "[INFO] code %d: %s\n", e->inotify_code, e->description);
+	}
+	if ((fd = inotify_init()) < 0) {
+		/* FIXME init failed */
+	}
+	if ((wd = inotify_add_watch(fd, ".", IN_CREATE)) < 0) {
+		/* FIXME add watch failed */
+	}
+	if (pthread_create(&data.tid, NULL, &handle, &data)) {
+		/* FIXME pthread create failed */
+	}
+	while (pthread_tryjoin_np(data.tid, NULL)) {
+		/* TODO stuff */
+	}
+	if (inotify_rm_watch(fd, wd)) {
+		/* FIXME remove watch failed */
+	}
+	if (close(fd)) {
+		/* FIXME close failed */
+	}
+	return EXIT_SUCCESS;
+}
